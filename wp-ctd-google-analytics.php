8d290c7186460d9d50bf8dc63650a83764e96716
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-11-14 19:12:42 +0100

    Reasonable working point.

--- wp-ctd-google-analytics.php
+++ wp-ctd-google-analytics.php
@@ -7,24 +7,20 @@
  */
 
 function wporg_settings_init() {
-	// Register a new setting for "wporg" page.
 	register_setting( 'wporg', 'wporg_options' );
 
-	// Register a new section in the "wporg" page.
 	add_settings_section(
-		'wporg_section_developers',
+		'ctd_section_developers',
 		__( 'CTD Google Analytics', 'wporg' ), 'wporg_section_developers_callback',
 		'wporg'
 	);
 
-	// Register a new field in the "wporg_section_developers" section, inside the "wporg" page.
 	add_settings_field(
-		'wporg_field_pill', // As of WP 4.6 this value is used only internally. // Use $args' label_for to populate the id inside the callback.
-			__( 'Pill', 'wporg' ),
+		'wporg_field_pill',
+        __( 'Pill', 'wporg' ),
 		'wporg_field_pill_cb',
 		'wporg',
-		'wporg_section_developers',
-		array( 'label_for' => 'wporg_field_pill', 'class' => 'wporg_row', 'wporg_custom_data' => 'custom' )
+		'ctd_section_developers'
 	);
 }
 add_action( 'admin_init', 'wporg_settings_init' );
@@ -35,29 +31,16 @@ function wporg_section_developers_callback( $args ) {
 	<?php
 }
 
-function wporg_field_pill_cb( $args ) {
+function wporg_field_pill_cb() {
 	$options = get_option( 'wporg_options' );
 	?>
-	<select
-			id="<?php echo esc_attr( $args['label_for'] ); ?>"
-			data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
-			name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
-		<option value="red" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'red', false ) ) : ( '' ); ?>>
-			<?php esc_html_e( 'red pill', 'wporg' ); ?>
-		</option>
- 		<option value="blue" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'blue', false ) ) : ( '' ); ?>>
-			<?php esc_html_e( 'blue pill', 'wporg' ); ?>
-		</option>
-	</select>
+    <textarea rows="4" cols="50" name="wporg_options"><?php echo isset( $options ) ? esc_attr( $options ) : ''; ?></textarea>
 	<p class="description">
 		<?php esc_html_e( 'The Google Analytics Tag, to appear in the head of each page.', 'wporg' ); ?>
 	</p>
 	<?php
 }
 
-/**
- * Add the top level menu page.
- */
 function wporg_options_page() {
 	add_menu_page(
 		'CTD Analytics',
@@ -69,17 +52,12 @@ function wporg_options_page() {
 }
 add_action( 'admin_menu', 'wporg_options_page' );
 
-/**
- * Top level menu callback function
- */
 function wporg_options_page_html() {
 	if ( ! current_user_can( 'manage_options' ) ) {
 		return;
 	}
 
-	// check if the user have submitted the settings // WordPress will add the "settings-updated" $_GET parameter to the url
 	if ( isset( $_GET['settings-updated'] ) ) {
-		// add settings saved message with the class of "updated"
 		add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
 	}
 
@@ -89,9 +67,7 @@ function wporg_options_page_html() {
 		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 		<form action="options.php" method="post">
 			<?php
-			// output security fields for the registered setting "wporg"
 			settings_fields( 'wporg' );
-			// output setting sections and their fields. (sections are registered for "wporg", each field is registered to a specific section)
 			do_settings_sections( 'wporg' );
 			submit_button( 'Save Settings' );
 			?>
